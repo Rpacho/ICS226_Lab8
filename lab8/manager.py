3089706598f944bbf498103e2e768af0226a55f6
Rolando <takaiyama00@gmail.com>
2019-11-18 11:48:41 -0800

    Ready for demo

--- lab8/manager.py
+++ lab8/manager.py
@@ -1,19 +1,35 @@
+#!/usr/bin/python3
+
 import asyncio
 
 PORT = 12345
-HOST = '::'
+# HOST = '::'
 
 
 async def client():
-    while (True):
-        reader, writer = await asyncio.open_connection(HOST, PORT)
-        inputCommand = input("What command do you want to be send (type: disk, memory, users): ")
-        writer.write(inputCommand.encode('utf-8') + b'\n')
-        await writer.drain()
+    AgentList = open("agent.conf", "r")
+    AgentHost = AgentList.readline()
+    inputCommand = input("What command do you want to be send (type: disk, memory, users): ")
+    while (AgentHost != ""):
+        try:
+            HOST = AgentHost.rstrip('\n')
+            reader, writer = await asyncio.open_connection(HOST, PORT)
 
-        output = await reader.read()
-        print(output.decode('utf-8'))
-    # writer.close()
-    # await writer.wait_close()
+            print("IP: ", HOST)
+            writer.write(inputCommand.encode('utf-8') + b'\n')
+            await writer.drain()
+
+            output = await reader.read()
+            print(output.decode('utf-8'))
+
+            AgentHost = AgentList.readline()
+            writer.close()
+            await writer.wait_closed()
+        except:
+            print("Can't connect to one of the Agents.")
+            writer.close()
+            await writer.wait_closed()
+            break
+
 
 asyncio.run(client())
